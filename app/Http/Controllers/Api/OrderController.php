b0819bc60c8509fb8647e10fe84dce79dd2f30cf
iwanhermawan29 <ihermawan227@gmail.com>
2024-06-28 07:55:06 +0700

    penambahan endpoint

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Order;
+use App\Models\OrderItem;
+use App\Models\Product;
 
 class OrderController extends Controller
 {
@@ -22,7 +25,164 @@ class OrderController extends Controller
         $user = $request->user();
         $data = $request->all();
         $data['user_id'] = $user->id;
+        $shippingAddress = $user->address;
+        $data['shipping_address'] = $shippingAddress;
+        $shippingLatLong = $user->latlong;
+        $data['shipping_latlong'] = $shippingLatLong;
+        $data['status'] = 'pending';
 
         $order = Order::create($data);
+
+        foreach ($request->order_items as $item) {
+            $product = Product::find($item['product_id']);
+            $orderItem = new OrderItem([
+                'quantity' => $item['quantity'],
+                'price' => $product->price,
+
+            ]);
+            $order->items()->save($orderItem);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Order created succesfully',
+            'data' =>  $order
+        ]);
+    }
+
+    //update purchase status
+    public function updatePurchaseStatus(Request $request, $id)
+    {
+        $request->validate([
+            'status' => 'required|string|in:pending,processing,completed,cancelled',
+        ]);
+
+        $order = Order::find($id);
+        $order->status = $request->status;
+        $order->save();
+
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Order status updated succesfully',
+            'data' =>  $order
+        ]);
+    }
+
+    //order history
+    public function orderHistory(Request $request)
+    {
+        $user = $request->user();
+        $orders = Order::where('user_id', $user->id)->get();
+
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Get all order history',
+            'data' =>  $orders
+        ]);
+    }
+
+    //cancel order
+    public function cancelOrder(Request $request, $id)
+    {
+        $order = Order::find($id);
+        $order->status = 'cancelled';
+        $order->save();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Order cancelled successfully',
+            'data' =>  $order
+        ]);
+    }
+
+    //get orders by status for restaurant
+    public function getOrderByStatus(Request $request)
+    {
+        $request->validate([
+            'status' => 'required|string|in:pending,processing,completed,cancelled',
+        ]);
+
+        $user = $request->user();
+        $orders = Order::where('restaurant_id', $user->id)
+            ->where('status', $request->status)
+            ->get();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Get all orders by status',
+            'data' =>  $orders
+        ]);
+    }
+
+    //update order status for restaurant
+    public function updateOrderStatus(Request $request, $id)
+    {
+        $request->validate([
+            'status' => 'required|string|in:pending,processing,completed,cancelled, ready_for_delivery, prepared',
+        ]);
+
+        $order = Order::find($id);
+        $order->status = $request->status;
+        $order->save();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Order status updated succesfully',
+            'data' =>  $order
+        ]);
+    }
+
+    //get orders by status for driver
+    public function getOrderByStatusDriver(Request $request)
+    {
+        $request->validate([
+            'status' => 'required|string|in:pending,processing,completed,cancelled, ready_for_delivery, prepared',
+        ]);
+
+        $user = $request->user();
+        $orders = Order::where('driver_id', $user->id)
+            ->where('status', $request->status)
+            ->get();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Get all orders by status',
+            'data' =>  $orders
+        ]);
+    }
+
+    //get order status ready for delivery
+    public function getOrderStatusReadyForDelivery(Request $request)
+    {
+
+        $orders = Order::with('restaurant')
+            ->where('status', 'ready_for_delivery')
+            ->get();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Get all orders by status ready for delivery',
+            'data' =>  $orders
+        ]);
+    }
+
+    //get orders by status for driver
+    public function UpdateOrderStatusDriver(Request $request, $id)
+    {
+        $request->validate([
+            'status' => 'required|string|in:pending,processing,completed,cancelled, on_the_way, delivered',
+        ]);
+
+        $order = Order::find($id);
+        $order->status = $request->status;
+        $order->save();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Get all orders by status',
+            'data' =>  $order
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -29,3 +29,18 @@ Route::put('/user/update-latlong', [App\Http\Controllers\Api\AuthController::cla
 Route::get('/restaurant', [App\Http\Controllers\Api\AuthController::class, 'getRestaurant']);
 
 Route::apiResource('/products', App\Http\Controllers\Api\ProductController::class)->middleware('auth:sanctum');
+
+//order
+Route::post('/order', [App\Http\Controllers\Api\OrderController::class, 'createOrder'])->middleware('auth:sanctum');
+//update purchase status
+Route::put('/order/{id}', [App\Http\Controllers\Api\OrderController::class, 'updatePurchaseStatus'])->middleware('auth:sanctum');
+//get order by user id
+Route::get('/order/user', [App\Http\Controllers\Api\OrderController::class, 'orderHistory'])->middleware('auth:sanctum');
+//get order by restaurant id
+Route::get('/order/restaurant', [App\Http\Controllers\Api\OrderController::class, 'getOrderByStatus'])->middleware('auth:sanctum');
+//get order by driver id
+Route::get('/order/driver', [App\Http\Controllers\Api\OrderController::class, 'getOrderByStatusDriver'])->middleware('auth:sanctum');
+//update order status
+Route::put('/order/restaurant/update-status/{id}', [App\Http\Controllers\Api\OrderController::class, 'updateOrderStatus'])->middleware('auth:sanctum');
+//update order status Driver
+Route::put('/order/driver/update-status/{id}', [App\Http\Controllers\Api\OrderController::class, 'updateOrderStatusDriver'])->middleware('auth:sanctum');
